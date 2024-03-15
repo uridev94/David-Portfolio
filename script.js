4f86eef57bdec075e8ae413af7c32608b382efea
Uriel Morales <urieldavid94@gmail.com>
2024-03-14 22:33:28 -0500

    Proyecto con estilos y html terminado con minimos detalles pendientes por corregir y faltante de agregar el script de JS

---
+++ script.js
@@ -0,0 +1,13 @@
+alert();
+let menuVisible = false;
+//Función para ocultar o mostrar el menú//
+
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
