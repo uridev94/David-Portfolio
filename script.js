b36a17393e31309e41d920597a9fc765f80a4d70
Uriel Morales <urieldavid94@gmail.com>
2024-03-14 23:43:53 -0500

    Agregando funcionalidades finales de JS

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-alert();
 let menuVisible = false;
 //Función para ocultar o mostrar el menú//
 
@@ -11,3 +10,26 @@ function mostrarOcultarMenu(){
         menuVisible = true;
     }
 }
+
+function seleccionar(){
+    //ocultar el menu una vez que  selecciono una opción
+    document.getElementById("nav").classList="";
+    menuVisible = false;
+}
+
+//Función con variable de scope global que aplica a las animaciones de las habilidades, detectando el scroll para desarrollar la animación de las barras.
+
+function efectoHabilidades(){
+    var skills = document.getElementById("skills")
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills>=300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[1].classList.add("javascript");
+    }
+}
+//Método para detectar el scrolling cuando pase por la sección de habilidades
+
+window.onscroll = function(){
+    efectoHabilidades();
+}
+
